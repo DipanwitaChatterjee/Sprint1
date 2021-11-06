92b6851f632c2bab97c0ea49676c63022618950d
Akshaycheruku <91118726+Akshaycheruku@users.noreply.github.com>
2021-11-06 18:28:35 +0530

    Update RespondentDao.java

--- OnlineSurveyBuilderApplication/lib/src/main/java/com/surveybuilder/dao/RespondentDao.java
+++ OnlineSurveyBuilderApplication/lib/src/main/java/com/surveybuilder/dao/RespondentDao.java
@@ -1,5 +1,21 @@
 package com.surveybuilder.dao;
 
-public interface RespondentDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+
+import com.surveybuilder.entities.Respondent;
+
+
+
+
+@Repository
+public interface RespondentDao extends JpaRepository<Respondent, Integer>{
+	@Query("select r from Respondent r where r.respondentId = ?1")
+	public Respondent findRespondentById(int id);
+
+	@Query("select a from Respondent a where (a.respondentUsername  = ?1 and a.respondentPassword = ?2)")
+	public Respondent authRespondent(String RespondentUsername, String pass);
 }
+
