6ba81170d0cb96d542fe011518889a63af5d22e0
Sanidhya Dandwate <48010478+sanidhya1108@users.noreply.github.com>
2021-11-06 17:56:46 +0530

    Update QuestionsDao.java

--- OnlineSurveyBuilderApplication/lib/src/main/java/com/surveybuilder/dao/QuestionsDao.java
+++ OnlineSurveyBuilderApplication/lib/src/main/java/com/surveybuilder/dao/QuestionsDao.java
@@ -1,5 +1,18 @@
 package com.surveybuilder.dao;
 
-public interface QuestionsDao {
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.surveyBuilder.entities.Questions;
+
+public interface QuestionsDao extends JpaRepository<Questions, Integer>{
+  
+  @Modifying
+	@Transactional
+	@Query(value="delete from Questions q where q.questionId=:questionId")
+	public void deleteQuestionById(@Param("questionId") int questionId);
 
 }
