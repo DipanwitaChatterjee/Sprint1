86e3dac30c2e70b1f25766ff8b8aa14e7c322911
Sanidhya Dandwate <48010478+sanidhya1108@users.noreply.github.com>
2021-11-06 18:03:43 +0530

    Update SurveyorService.java

--- OnlineSurveyBuilderApplication/lib/src/main/java/com/surveybuilder/service/SurveyorService.java
+++ OnlineSurveyBuilderApplication/lib/src/main/java/com/surveybuilder/service/SurveyorService.java
@@ -1,5 +1,19 @@
 package com.surveybuilder.service;
 
+import com.surveybuilder.entities.Feedback;
+import com.surveybuilder.entities.Questions;
+import com.surveybuilder.entities.Responses;
+import com.surveybuilder.entities.Survey;
+
 public interface SurveyorService {
 
+  public void addSurvey(Survey survey);
+	public void addQuestion(Questions questions);
+	public List<Questions> findAllSurvey();
+	public Survey findSurveyById(int surveyId);
+	public void updateQuestion(int questionId, Questions questions);
+	public void removeQuestion(int questionId);
+	public void removeSurvey(int surveyId);
+	public List<Responses> findResponsesById(int respondentId);
+	public List<Feedback> findAllFeedback();
 }
